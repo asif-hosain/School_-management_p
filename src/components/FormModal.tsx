ee68e344af8708bf4519eee98249b52c331c7dde
asif-hosain <asifhdoha@gmail.com>
2024-09-26 21:15:56 +0600

    teacher create form full

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import TeacherForm from "./forms/TeacherForm";
 
 const FormModal = ({table,type,data,id}:{
     table:"teacher" | "student" | "parent" | "subject" |  "class" |  "lesson" |  "exam" |  "assignment" |  "result" |  "attendance" |  "announcement" ;  
@@ -27,7 +28,7 @@ const FormModal = ({table,type,data,id}:{
             <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
           </form>
         ) : (
-          "create or update form"
+          <TeacherForm type="create"/>
         );
       }
 

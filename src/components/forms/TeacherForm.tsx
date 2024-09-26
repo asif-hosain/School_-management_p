a54e19c5ce094c495c0f0a970f3be1a4522a8518
asif-hosain <asifhdoha@gmail.com>
2024-09-26 16:45:16 +0600

    Modal Content

---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,15 @@
+"use client"
+const TeacherForm = ({
+    type,
+    data,
+    
+}: {type: "create" | "update"; 
+    data?: any;
+
+}) => {
+  return (
+    <form className="">input</form>
+  ) 
+}
+
+export default TeacherForm

e42df9120ccdef247b7cb3599c67ca6897ec210a
asif-hosain <asifhdoha@gmail.com>
2025-02-03 16:13:17 +0600

    Route hompage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,41 @@
-const Homepage = () => {
+import Announcement from "@/components/Announcement"
+import AtendanceChart from "@/components/AtendanceChart"
+import CountCharts from "@/components/CountCharts"
+import EventCalendar from "@/components/EventCalendar"
+import FinanceChart from "@/components/FinanceChart"
+import UserCard from "@/components/UserCard"
+
+const AdminPage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className='flex gap-4 p-4 flex-col  md:flex-row'>
+      {/* {left} */}
+      <div className="w-fill lg:w-2/3 flex flex-col gap-8">
+      {/* {UserCard} */}
+      <div className="flex gap-4 justify-between flex-wrap">
+        <UserCard type="student"/>
+        <UserCard type="teacher"/>
+        <UserCard type="parent"/>
+        <UserCard type="staff"/>
+      </div>
+      {/* {middle Chart } */}
+      <div className="flex gap-4 flex-col lg:flex-row">
+        {/* {count chart} */}
+        <div className="w-full lg:w-1/3 h-[450px]"><CountCharts/></div>
+        {/* {attendance chart } */}
+        <div className="w-full lg:w-2/3 h-[450px]"><AtendanceChart/></div>
+      </div>
+      {/* {bottomchart} */}
+      <div className="w-full h-[500px]">
+        <FinanceChart/>
+      </div>
+      </div>
+      {/* {right} */}
+      <div className="w-fill lg:w-1/3 flex flex-col gap-8 ">
+      <EventCalendar/>
+      <Announcement/>
+      </div>
+    </div>
   )
 }
 
-export default Homepage
+export default AdminPage
